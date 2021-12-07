0ce6702d423a044c5acc5087c4c108c270dcd422
117100 <zhangerjiong@wywk.cn>
2021-12-07 19:12:40 +0800

    requests

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
-#coding=utf-8
+#coding=utf-8、
+from selenium import webdriver
+from time import sleep
+import requests
+d=webdriver.Chrome()
+d.get("https://eoms-test.wywk.cn/dashbord")
+d.find_element_by_id("username").send_keys("117100")
+d.find_element_by_id("password").send_keys("jiahui0819....")
+d.find_element_by_id("login-submit").click()
+sleep(8)
+d.get("https://eoms-test.wywk.cn/pms-management/hotel")
+sleep(2)
 import pytest
 a=9461
 b=2047
